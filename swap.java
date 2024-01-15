0933c06ceedb11719e3dd246331eaa3987d60977
swapnil_ranjan <swapnilranjan181200@gmail.com>
2024-01-15 15:52:31 +0530

    push

---
+++ swap.java
@@ -0,0 +1,7 @@
+public class swap {
+        public static void main(String args[]){
+            String s ="Swapnil Ranjan";
+            System.out.println("The substring is:" +s.substring(0,7));
+            }
+        }
+
